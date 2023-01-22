006bc6a3ccd0ccaf88fd6455266c8f7cad9318bf
Arthur Werle <arthur.werle@gmail.com>
2023-01-22 11:38:20 -0300

    add SSR, add meta description & remove unused plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-    },
-  },
-  integrations: [preact(), tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
