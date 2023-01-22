644a5e8c95fa464cd5571317a31076eb4796f541
Arthur Werle <arthur.werle@gmail.com>
2023-01-22 10:10:54 -0300

    finishing layout

---
+++ src/constants/readings.ts
@@ -0,0 +1,46 @@
+export const READINGS = [
+  { title: "as dores do mundo", author: "schopenhauer", rating: 2 },
+  {
+    title: "who's in charge?: free will and the science of the brain",
+    author: "michael s. gazzaniga",
+    rating: 4,
+  },
+  { title: "sobre o amor", author: "carl gustav jung", rating: 2 },
+  { title: "sobre a breviedade da vida", author: "sêneca", rating: 5 },
+  { title: "amor líquido", author: "bauman", rating: 3 },
+  { title: "steal like an artist", author: "austin kleon", rating: 5 },
+  {
+    title: "segredos do império romano",
+    author: "machado de assis",
+    rating: 4,
+  },
+  {
+    title: "a verdadeira história da inquisição",
+    author: "rino camilleri",
+    rating: 2,
+  },
+  { title: "líderes que mudaram o mundo", author: "gordon kerr", rating: 2 },
+  {
+    title: "como os paises ricos ficaram ricos",
+    author: "erik s. reinert",
+    rating: 5,
+  },
+  { title: "fascismo à brasileira", author: "pedro doria", rating: 5 },
+  { title: "como vejo o mundo", author: "albert einstein", rating: 4 },
+  {
+    title: "china, o socialismo do século XXI",
+    author: "elias jabbour",
+    rating: 3,
+  },
+  {
+    title: "1499 o brasil antes de cabral",
+    author: "reinaldo josé lopes",
+    rating: 4,
+  },
+  {
+    title:
+      "trabalho: uma história de como utilizamos o nosso tempo: da idade da pedra à era dos robôs",
+    author: "james suzman",
+    rating: 5,
+  },
+]
